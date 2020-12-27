b4142536f86e45b9c0407c9c600a4c804fbdcc97
lllToTolll <69581325+to63@users.noreply.github.com>
2020-12-27 12:57:46 +0700

    ทำ enntity สำหรับเบ ระบบการนัดตรวจโรคกระดูก issue - close #2

---
+++ backend/ent/schema/bonedisease.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// Bonedisease holds the schema definition for the Bonedisease entity.
+type Bonedisease struct {
+	ent.Schema
+}
+
+// Fields of the Bonedisease.
+func (Bonedisease) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the Bonedisease.
+func (Bonedisease) Edges() []ent.Edge {
+	return nil
+}
